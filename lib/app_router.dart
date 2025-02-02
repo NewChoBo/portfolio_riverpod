7016f8d60c4105cd0c7efd78ebae05153207073d
NewChoBo <whwjdrb1009@gmail.com>
2025-02-02 21:53:18 +0900

    fix: 코드 정리 및 수정

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 import 'package:portfolio_riverpod/home/presentation/screens/home_screen.dart';
 import 'package:portfolio_riverpod/post/presentation/screens/post_screen.dart';
 import 'package:portfolio_riverpod/user/presentation/screens/user_screen.dart';
--- lib/post/presentation/widgets/post_widget.dart
+++ lib/post/presentation/widgets/post_widget.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/material.dart';
 
 class PostWidget extends StatelessWidget {
+  const PostWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(

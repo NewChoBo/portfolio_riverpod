8f36d294695ebc271698dbdf46a05fe563e14191
NewChoBo <whwjdrb1009@gmail.com>
2025-02-02 21:59:44 +0900

    fix: 경로 수정

--- lib/global/data/repositories/global_repository_impl.dart
+++ lib/global/data/repositories/global_repository_impl.dart
@@ -1,4 +1,4 @@
 // TODO: Global 기능의 리포지토리를 구현하세요.
-import 'package:portfolio_riverpod/home/global/domain/repositories/global_repository.dart';
+import 'package:portfolio_riverpod/global/domain/repositories/global_repository.dart';
 
 class GlobalRepositoryImpl implements GlobalRepository {}

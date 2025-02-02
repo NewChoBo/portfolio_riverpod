6245a9e5cb0ae75999c6a3bf9464327a73c7ab20
NewChoBo <whwjdrb1009@gmail.com>
2025-02-02 16:41:28 +0900

    fix: 해시 기반 url 주소 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:portfolio_riverpod/app_router.dart';
 
 void main() {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
